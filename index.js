52968257d4094c06049656f114888fee10c98363
Yakumwamba <brianlemba@gmail.com>
2022-09-29 07:11:00 +0200

    encrypt

--- index.js
+++ index.js
@@ -85,7 +85,9 @@ const challengeRouter = require('./routes/challenge');
 const quizRouter = require('./routes/quiz');
 const reportRouter=require('./routes/reports');
 const busboy = require('connect-busboy')
-
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -99,6 +101,28 @@ app.use('/api/v1/teacher',teacherRouter)
 app.use('/api/v1/quiz',quizRouter)      
 app.use('/api/v1/reports',reportRouter) 
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+// app.listen(port, () => {
+//     console.log('Server is up on port ' + port)
+// })
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/thutotimequiz.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/thutotimequiz.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
