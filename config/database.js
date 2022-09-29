c4b22663d23b050406f60756186da42a5a0b781b
Yakumwamba <brianlemba@gmail.com>
2022-09-29 09:54:08 +0200

    data

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var gracefulShutdown;
-var dbURI = 'mongodb://localhost:27017/Thutoquiz';
+var dbURI = 'mongodb://localhost:27017/ThutoQuizs';
 // if (process.env.NODE_ENV === 'production') {
 //   dbURI = process.env.MONGOLAB_URI;
 // }
