60b5a540e75bac296dbcf76b02c72f2f15583437
Yakumwamba <brianlemba@gmail.com>
2022-09-29 07:13:59 +0200

    debug

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.use('/api/v1/reports',reportRouter)
 // Certificate
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/thutotimequiz.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/thutotimequiz.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/thutotimequiz.com/chain.pem', 'utf8');
 
 const credentials = {
 	key: privateKey,
